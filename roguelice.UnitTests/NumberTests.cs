8096f651934f7cb301f3756d8a3f62f8c45d3dc9
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-10-30 15:24:52 +0100

    finishing tests for Clamp

--- roguelice.UnitTests/NumberTests.cs
+++ roguelice.UnitTests/NumberTests.cs
@@ -7,11 +7,19 @@ namespace roguelice.UnitTests
     public class NumberTests
     {
         [TestMethod]
-        public void Clamp_ReturnsClampedNumber()
+        public void Clamp_ReturnsClampedPositive()
         {
             Assert.AreEqual(Numbers.Clamp(1, 0, 2), 1);
-            Assert.AreEqual(Numbers.Clamp(1, -3, -1), -1);
+            Assert.AreEqual(Numbers.Clamp(1, 5, 10), 5);
             Assert.AreEqual(Numbers.Clamp(30, 10, 20), 20);
+        }
+
+        [TestMethod]
+        public void Clamp_ReturnsClampedNegative()
+        {
+            Assert.AreEqual(Numbers.Clamp(-1, -2, 0), -1);
+            Assert.AreEqual(Numbers.Clamp(-20, -15, -10), -15);
+            Assert.AreEqual(Numbers.Clamp(-30, -25, -20), -25);
         }
     }
 }
