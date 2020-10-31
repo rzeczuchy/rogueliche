1144586d5c8a22b8f78ca384f85b0aad5a825e06
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-10-31 14:50:20 +0100

    removing unnecessary usings, formatting

--- roguelice/Graphics.cs
+++ roguelice/Graphics.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace roguelice
 {
     public class Graphics
     {
-
         private readonly char[][] buffer;
 
         public Graphics()
