4b707fbb1f10ea558262afeae42de12b515dbc83
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2020-11-29 10:04:16 +0100

    adding reached floor display to game over screen

--- roguelice/GameOverState.cs
+++ roguelice/GameOverState.cs
@@ -31,6 +31,7 @@ namespace roguelice
         {
             render.DrawCenteredString("You died.", render.Height / 2);
             render.DrawCenteredString("You killed " + player.KillCount + " beasts and attained level " + player.Lvl + ".", render.Height / 2 + 1);
+            render.DrawCenteredString("You reached " + player.Location.Name + ".", render.Height / 2 + 2);
             render.DrawCenteredString("You broke " + player.BrokenWeapons + " weapons.", render.Height / 2 + 3);
             render.DrawCenteredString("Press ENTER to restart.", render.Height / 2 + 4);
 
