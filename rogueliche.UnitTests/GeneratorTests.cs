a4a917120908dd53a0d21d32d672b315b5ac1de7
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-01-17 11:54:39 +0100

    adding GeneratorTests

---
+++ rogueliche.UnitTests/GeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rogueliche.UnitTests
+{
+    [TestClass]
+    public class GeneratorTests
+    {
+        [TestMethod]
+        public void NewMonster_ReturnsMonster()
+        {
+            var generator = new Generator();
+        }
+
+        [TestMethod]
+        public void NewWeapon_ReturnsWeapon()
+        {
+
+        }
+    }
+}
