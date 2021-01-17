277ed1ab80cd8d78279de69c2da70c3a9f0e18c5
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-01-17 23:43:15 +0100

    adding TilemapTests.cs

---
+++ rogueliche.UnitTests/TilemapTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rogueliche.UnitTests
+{
+    [TestClass]
+    public class TilemapTests
+    {
+        [TestMethod]
+        public void GetTile_ReturnsTile()
+        {
+            var mapSize = new Point(12, 23);
+            var tilemap = new Tilemap(null, mapSize.X, mapSize.Y);
+            tilemap.FillWithTile(tilemap.Bounds, Tile.TileType.floor);
+
+            Assert.AreEqual(tilemap.GetTile(new Point(mapSize.X - 1, mapSize.Y - 1)).Type, Tile.TileType.floor);
+            Assert.AreEqual(tilemap.GetTile(new Point(mapSize.X + 1, mapSize.Y + 1)), null);
+        }
+    }
+}
