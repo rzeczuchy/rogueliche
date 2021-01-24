3a80f931b9d40a9145479cbdede056cbc9c08ed0
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-01-24 11:27:40 +0100

    adding Dungeon tests

---
+++ rogueliche.UnitTests/DungeonTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rogueliche.UnitTests
+{
+    [TestClass]
+    public class DungeonTests
+    {
+        [TestMethod]
+        public void NewLevel_ReturnsLocation()
+        {
+            var dungeon = new Dungeon();
+            var level = dungeon.NewLevel();
+
+            Assert.IsInstanceOfType(level, typeof(ILocation));
+        }
+
+        [TestMethod]
+        public void NewLevel_ChangesIndex()
+        {
+            var dungeon = new Dungeon();
+            var level1 = dungeon.NewLevel() as DungeonLevel;
+            var level2 = dungeon.NewLevel() as DungeonLevel;
+
+            Assert.AreEqual(level1.LevelIndex, 1);
+            Assert.AreEqual(level2.LevelIndex, 2);
+        }
+    }
+}
