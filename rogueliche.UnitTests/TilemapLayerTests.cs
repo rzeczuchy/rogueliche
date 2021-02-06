d666f3a34e359f08087cf6ccc1162454fbd6ba54
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-02-06 19:17:21 +0100

    adding TilemapLayerTests

---
+++ rogueliche.UnitTests/TilemapLayerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rogueliche.UnitTests
+{
+    [TestClass]
+    public class TilemapLayerTests
+    {
+        [TestMethod]
+        public void TilemapLayer_Initializes()
+        {
+            try
+            {
+                var dungeon = new Dungeon();
+                var level = dungeon.NewLevel();
+                var tilemap = new Tilemap(level, 100, 100);
+                new TilemapLayer(tilemap);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void Constructor_ThrowsExceptionWhenTilemapNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => { new TilemapLayer(null); });
+        }
+    }
+}
