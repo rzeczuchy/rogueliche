635224589eee123c9363f523c81abc28c7a2d907
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-02-12 20:25:59 +0100

    more weapon tests

--- rogueliche.UnitTests/WeaponTests.cs
+++ rogueliche.UnitTests/WeaponTests.cs
@@ -11,5 +11,19 @@ namespace rogueliche.UnitTests
         {
             Assert.ThrowsException<ArgumentNullException>(() => { new Weapon(null, new Point(0, 0), null, null); });
         }
+
+        [TestMethod]
+        public void Weapon_Initializes()
+        {
+            var type = new WeaponType("name", 'c', 10, 10, 10);
+            try
+            {
+                var weapon = new Weapon(null, new Point(0, 0), type, null);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
     }
 }
