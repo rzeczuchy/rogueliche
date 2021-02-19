bbd3f547cd6324d863f2bb2ee8b6e5a1959a016a
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-02-19 08:53:50 +0100

    adding weapon tests

--- rogueliche.UnitTests/WeaponTests.cs
+++ rogueliche.UnitTests/WeaponTests.cs
@@ -35,5 +35,37 @@ namespace rogueliche.UnitTests
 
             Assert.AreEqual(weapon.Damage, 10);
         }
+
+        [TestMethod]
+        public void DecreaseDurability_WorksWhenPlayerNull()
+        {
+            var type = new WeaponType("name", 'c', 10, 10, 10);
+            var weapon = new Weapon(null, new Point(0, 0), type, null);
+
+            try
+            {
+                weapon.DecreaseDurability(1, null);
+                weapon.DecreaseDurability(weapon.Durability, null);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+        }
+
+        [TestMethod]
+        public void DecreaseDurability_DecreasesWhenNotBroken()
+        {
+            int durability = 10;
+            var type = new WeaponType("name", 'c', 10, 10, durability);
+            var weapon = new Weapon(null, new Point(0, 0), type, null);
+
+            weapon.DecreaseDurability(1, null);
+            Assert.AreEqual(weapon.Durability, durability - 1);
+
+            weapon.DecreaseDurability(weapon.Durability, null);
+            Assert.AreEqual(weapon.Durability, 0);
+        }
     }
 }
