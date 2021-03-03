5a2ba341bb96c5aa5bcd07b9823633d3e4b16ed6
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-03-03 20:12:26 +0100

    refactoring SaveHandler

--- rogueliche/SaveHandler.cs
+++ rogueliche/SaveHandler.cs
@@ -16,20 +16,7 @@ namespace rogueliche
         public void SaveGame(Player player)
         {
             DeleteSave();
-
-            var save = new Save()
-            {
-                PlayerKillCount = player.KillCount,
-                PlayerBrokenWeapons = player.BrokenWeapons,
-                PlayerMaxHealth = player.MaxHealth,
-                PlayerHealth = player.Health,
-                PlayerLvl = player.Lvl,
-                PlayerExp = player.Exp,
-                PlayerExpToNextLvl = player.ExpToNextLvl,
-                PlayerMaxExertion = player.MaxExertion,
-                PlayerExertion = player.Exertion,
-            };
-
+            var save = CreateSave(player);
             var serializer = new XmlSerializer(save.GetType());
             var settings = new XmlWriterSettings { Indent = true };
             using (var writer = XmlWriter.Create(Savepath, settings))
@@ -41,21 +28,18 @@ namespace rogueliche
         public Save LoadGame()
         {
             var save = new Save();
-
             var serializer = new XmlSerializer(save.GetType());
             using (var reader = XmlReader.Create(Savepath))
             {
-                if (serializer.CanDeserialize(reader))
+                if (CanUnpackSaveFile(reader, serializer))
                 {
-                    save = (Save)serializer.Deserialize(reader);
+                    return UnpackedSave(serializer, reader);
                 }
                 else
                 {
                     throw new Exception("Could not deserialize save file to a Save object.");
                 }
             }
-
-            return save;
         }
 
         public bool CanLoadGame()
@@ -69,6 +53,32 @@ namespace rogueliche
             {
                 File.Delete(Savepath);
             }
+        }
+
+        private Save CreateSave(Player player)
+        {
+            return new Save()
+            {
+                PlayerKillCount = player.KillCount,
+                PlayerBrokenWeapons = player.BrokenWeapons,
+                PlayerMaxHealth = player.MaxHealth,
+                PlayerHealth = player.Health,
+                PlayerLvl = player.Lvl,
+                PlayerExp = player.Exp,
+                PlayerExpToNextLvl = player.ExpToNextLvl,
+                PlayerMaxExertion = player.MaxExertion,
+                PlayerExertion = player.Exertion,
+            };
+        }
+
+        private bool CanUnpackSaveFile(XmlReader reader, XmlSerializer serializer)
+        {
+            return serializer.CanDeserialize(reader);
+        }
+
+        private Save UnpackedSave(XmlSerializer serializer, XmlReader reader)
+        {
+            return (Save)serializer.Deserialize(reader);
         }
 
         private bool SaveFileExists()
