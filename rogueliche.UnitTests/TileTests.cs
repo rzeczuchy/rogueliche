a90585f47764d9c3b4803878605fcb59df9d2d17
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-06-01 20:02:55 +0200

    adding TileTests

---
+++ rogueliche.UnitTests/TileTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rogueliche.UnitTests
+{
+    [TestClass]
+    public class TileTests
+    {
+        [TestMethod]
+        public void Tile_Initializes()
+        {
+            try
+            {
+                var tile = new Tile(Tile.TileType.wall);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
