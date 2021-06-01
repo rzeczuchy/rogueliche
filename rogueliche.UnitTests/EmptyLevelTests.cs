9dd52fc644d268004f8496bb4383838d1b8a5bc3
rzeczuchy <57368722+rzeczuchy@users.noreply.github.com>
2021-06-01 20:01:18 +0200

    adding EmptyLevelTests

---
+++ rogueliche.UnitTests/EmptyLevelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace rogueliche.UnitTests
+{
+    [TestClass]
+    public class EmptyLevelTests
+    {
+        [TestMethod]
+        public void EmptyLevel_Initializes()
+        {
+            try
+            {
+                var level = new EmptyLevel("test", 0, 0);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
